f7525f87b17f5c45d090a40c344f3452640f461b
Fredrik Liljegren <fredrik.liljegren@textalk.se>
2013-04-08 16:22:27 +0200

    Adding an spl autoloader.

---
+++ autoload.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Include this file to register an spl_autoload for these classes.
+ */
+
+// Define a shortcut constant for DIRECTORY_SEPARATOR
+if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+
+function sai_autoload($class) {
+  if (strpos($class, 'SAI_') !== 0) return; // Not a SAI-class.
+
+  $subclass = substr($class, 4);
+  $filename = dirname(__FILE__) . DS . 'lib' . DS . str_replace('_', DS, $subclass);
+
+  if (file_exists($filename)) require_once($filename);
+}
+
+spl_autoload_register('sai_autoload');
+
