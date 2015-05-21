f5439353097045fed5411bb68c2bf6675319f637
Joachim Ekstrand <joachim@reddot.reddot.red>
2015-05-21 11:42:09 +0200

    Added SAI\Response::setResponseCode() and unit tests.

--- lib/Response.php
+++ lib/Response.php
@@ -18,6 +18,12 @@ interface Response {
    */
   public function setHeader($name, $value);
 
+  /**
+   * Sets HTTP response code.
+   * @param int $code The HTTP response code.
+   */
+  public function setResponseCode($code);
+
   public function setCookie(
     $name, $value, $expire = 0, $path = '/', $domain = null, $secure = false, $httponly = false
   );
---
+++ tests/MockResponseTest.php
@@ -0,0 +1,49 @@
+<?php
+
+class MockResponseTest extends PHPUnit_Framework_TestCase {
+
+  private $response_mock;
+
+  public function setUp() {
+    $this->response_mock = new SAI\Mock\Response;
+  }
+
+  public function testSetHeader() {
+    $this->response_mock->setHeader('nAmE', 'value');
+    $this->assertArrayHasKey('name', $this->response_mock->headers);
+    $this->assertEquals('value',  $this->response_mock->headers['name']);
+  }
+
+  public function testResponseCode() {
+    $this->response_mock->setResponseCode('HTTP/1.1 303 See Other');
+    $this->assertEquals(
+      'HTTP/1.1 303 See Other',
+      $this->response_mock->response_code
+    );
+  }
+
+  public function testSetCookie() {
+    $this->response_mock->setCookie('name', 'value');
+    $this->assertArrayHasKey('name', $this->response_mock->cookies);
+    $this->assertEquals('value', $this->response_mock->cookies['name']);
+  }
+
+  public function testDeleteCookie() {
+    $this->response_mock->setCookie('name', 'value');
+    $this->response_mock->deleteCookie('name');
+
+    $this->assertArrayNotHasKey('name', $this->response_mock->cookies);
+    $this->assertContains('name', $this->response_mock->cookies_deleted);
+  }
+
+  public function testOutput() {
+    $this->response_mock->output('Test output');
+    $this->assertEquals('Test output', $this->response_mock->output);
+
+    $this->response_mock->output(' is continued.');
+    $this->assertEquals(
+      'Test output is continued.',
+      $this->response_mock->output
+    );
+  }
+}
